0ba895cbc8b6dc51c5cde7390066f863c0b72928
ola-sk <50403262+ola-sk@users.noreply.github.com>
2025-06-16 01:47:34 +0200

    Add color parameter to visualize_cube function for customizable edge colors

--- src/visualise_cube.py
+++ src/visualise_cube.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 from src.create_cube import create_cube
 import numpy as np
 
-def visualize_cube(cube: np.ndarray = create_cube(size=1)):
+def visualize_cube(cube: np.ndarray = create_cube(size=1), color: str = 'violet'):
     # Initialize the 3D plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     # Plot each edge
     for edge in cube:
-        ax.plot(edge[:, 0], edge[:, 1], edge[:, 2], color='violet')
+        ax.plot(edge[:, 0], edge[:, 1], edge[:, 2], color=color)
 
     # Set axis labels
     ax.set_xlabel('X')
