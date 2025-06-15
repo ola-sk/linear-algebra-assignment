51770abec7b233765cfc2dea55b06c7b218fba6d
ola-sk <50403262+ola-sk@users.noreply.github.com>
2025-06-16 01:47:34 +0200

    Implement rotation of cube edges using rotate function and visualize the transformed cube

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from vectors import Vector3D
 from src.create_cube import create_cube
 from src.visualise_cube import visualize_cube
-
+from src.transformations import rotate
+import numpy as np
 
 if __name__ == '__main__':
     origin = Vector3D.create([2, 2, 1])  # Use the create method
     cube = create_cube(1, origin)
-    visualize_cube(cube)
+
+    rotation_matrix = rotate(45, 0, 1)
+    rotation_matrix2 = rotate(45, 0, 2)
+    rotated_matrix = []
+    for edge in cube:
+        rotated_matrix.append(edge @ rotation_matrix.T) # equivalent to (rotation_matrix @ edge.T).T; we avoid one transpose
+    rotated_ndarray = np.array(rotated_matrix)
+    visualize_cube(rotated_ndarray)
