Fix rotation matrix application in cube edge transformation for correct visualization

src/main.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     rotation_matrix2 = rotate(45, 0, 2)
     rotated_matrix = []
     for edge in cube:
-        rotated_matrix.append(edge @ rotation_matrix.T) # equivalent to (rotation_matrix @ edge.T).T; we avoid one transpose
+        rotated_matrix.append((rotation_matrix @ edge.T).T)
     rotated_ndarray = np.array(rotated_matrix)
     visualize_cube(rotated_ndarray)